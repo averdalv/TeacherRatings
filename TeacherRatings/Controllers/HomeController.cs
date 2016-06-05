a6893777fe39041e9f443b87dff382c3bb3b5a6e
VladR <averdalv@gmail.com>
2016-06-06 01:33:34 +0300

    push on Azure

--- TeacherRatings/Controllers/HomeController.cs
+++ TeacherRatings/Controllers/HomeController.cs
@@ -13,16 +13,9 @@ namespace TeacherRatings.Controllers
         {
             
             var context = new DataContext();
-            var teach = context.Teachers;
-            ViewBag.Teachers = teach;
 
-            //Criteria c = new Criteria();
-            //c.Accessibility = c.ClarityImportance = c.CommunicationDisciplines = c.Enthusiasm =
-            //    c.Examples = c.ImproveMySkills = c.Insistence = c.Interest = c.ObjectivityAssessment = c.Preparedness =
-            //    c.Ratio = c.Visit = "";
-            //c.TeacherSubject = context.TeacherSubjects.First();
-            //context.Criterias.Add(c);
-            //context.SaveChanges();
+            var t = context.Teachers;
+            ViewBag.Teachers = t.ToList();  
             return View();
         }        
     }
